9508ac129abd0852d77b7e7063da31dc9ac612c1
galenmarchetti <galenmarchetti@gmail.com>
2023-10-25 14:00:35 -0500

    connecting to postgres

--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('postgresql://username:password@localhost:5432/mydatabase')
+
+df = pd.read_sql_query('select * from "my_table"',con=engine)
+
+st.write("Table: " + str(df))
 
 st.write("Hello World!")
