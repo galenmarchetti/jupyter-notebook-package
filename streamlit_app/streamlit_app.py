b789dec70ca786314379dab1bf7d6000aa84647f
galenmarchetti <galenmarchetti@gmail.com>
2023-10-26 09:32:57 -0500

    streamlit app

--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -8,15 +8,27 @@ from sqlalchemy import create_engine, text
 CONFIG_PATH = "config/config.json"
 
 with open(CONFIG_PATH) as config_fp:
-    config_file = json.load(config_fp)
+    config_json = json.load(config_fp)
 
-st.write(config_file)
+st.write(config_json)
 
-postgres_url = config_file['postgres_url']
-engine = create_engine(postgres_url)
+if 'postgres_url' in config_json:
+    postgres_url = config_json['postgres_url']
+    engine = create_engine(postgres_url)
 
-df = pd.read_sql_query('select * from demo_table',con=engine)
+    df = pd.read_sql_query('select * from demo_table',con=engine)
 
-st.write("Contents of 'demo_table' table:")
+    st.write("Contents of 'demo_table' table:")
 
-st.data_editor(df)
+    st.data_editor(df)
+
+if 'mongodb_url' in config_json:
+    mongodb_url = config_json['mongodb_url']
+    client = MongoClient(mongodb_url)
+
+    db = client['demo_db']
+    collection = db['demo_collection']
+
+    cursor = collection.find({})
+    for document in cursor:
+          print(document)
